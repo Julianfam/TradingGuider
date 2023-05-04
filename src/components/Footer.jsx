981b8053b8fd8e4d0467e3d1cb64fd78b379d9ce
Julian Andrade <julifeli123@gmail.com>
2023-05-04 09:09:04 -0500

    footer message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import styles from '@styles/Footer.module.scss';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.messagefooter}>KEEP IT SIMPLE</div>
   )
 }
 
